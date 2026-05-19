9bc22d1a1b77ab54be1be0725c7dc2d7bdde9299
diogobonet <diogosobezak@gmail.com>
2026-05-19 17:55:52 -0300

    chore: configure modular structure and bootstrap routing
    Signed-off-by: diogobonet <diogosobezak@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Modules\Banking\Core\Interfaces\AccountRepositoryInterface;
+use App\Modules\Banking\Infrastructure\Repositories\InMemoryAccountRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            AccountRepositoryInterface::class,
+            InMemoryAccountRepository::class
+        );
     }
 
     /**
